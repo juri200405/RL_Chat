d699356a591a6fcd33fb8815b3e9f70a6dfc978f
juri200405 <juri200405@gmail.com>
2020-10-17 11:50:05 +0900

    コーパスから重複文を削除

--- contents/transformer_pretrain.sh
+++ contents/transformer_pretrain.sh
@@ -2,7 +2,7 @@
 
 python 	src/run_model.py \
 	-m data/tomioka/courpus_model.model \
-	-i data/tomioka/courpus.txt \
+	-i data/tomioka/courpus_uniq.txt \
 	-p data/tomioka/transformer_config/vae_transformer.json \
 	-b data/tomioka/laboroai_bert/large/converted/ \
-	-o data/tomioka/output/transformer_vae/2020_10_16_03/
+	-o data/tomioka/output/transformer_vae/2020_10_17_02/
