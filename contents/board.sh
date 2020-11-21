d6b2c54fe80cb2995343fb94b3c0f86a441e4e0f
juri200405 <juri200405@gmail.com>
2020-11-22 06:25:58 +0900

    データ作成スクリプトの微修正

--- contents/board.sh
+++ contents/board.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 ~/conda/envs/pl/bin/tensorboard \
-	--logdir data/output/transformer_vae/2020_11_19/ \
+	--logdir data/output/transformer_vae/2020_11_21/ \
 	--port 31096 --bind_all
--- contents/make_data.sh
+++ contents/make_data.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 python 	src/bert_data.py \
-	-m data/tomioka/normalize_model.model \
-	-i data/tomioka/normalize.txt \
-	-o data/tomioka/normalize.pkl
+	-m data/tomioka/spm_model/no_kwdlc.model \
+	-i data/tomioka/no_kwdlc.txt \
+	-o data/tomioka/no_kwdlc_126.pkl \
+	-n 1 \
+	-l 126
