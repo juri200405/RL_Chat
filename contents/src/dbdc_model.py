33a9dcba10c141e56a3f1f890029f1f3314397e1
juri200405 <juri200405@gmail.com>
2020-11-28 20:35:27 +0900

    dbdc_modelの学習の実装

--- contents/src/dbdc_model.py
+++ contents/src/dbdc_model.py
@@ -5,6 +5,7 @@ import json
 import torch
 import sentencepiece as spm
 import tqdm
+import numpy as np
 
 from encoder_decoder import transformer_Encoder, Transformer_Embedding
 from config import Config
@@ -26,10 +27,9 @@ class DBDC(torch.nn.Module):
     def forward(self, x, mask, conv_len):
         with torch.no_grad():
             x = self.encoder(x, attention_mask=mask)
-            print(x)
             x = x.view(conv_len.shape[0], -1, x.shape[1])
 
-        hidden = torch.zeros(1, x.shape[0], self.hidden_size)
+        hidden = torch.zeros(1, x.shape[0], self.hidden_size, device=x.device)
         outputs, _ = self.gru(x, hidden)
 
         idx = (conv_len-1).view(-1, 1).expand(outputs.shape[0], outputs.shape[2]).unsqueeze(1)
@@ -69,7 +69,7 @@ def get_collate_fn():
         padded_tensors = torch.cat([torch.cat((item, torch.full((max_conv_len -item.shape[0], item.shape[1]), 3, dtype=torch.long)), dim=0) for item in padded_tensors], dim=0)
         padding_masks = torch.cat([torch.cat((item, torch.full((max_conv_len -item.shape[0], item.shape[1]), True, dtype=torch.bool)), dim=0) for item in padding_masks], dim=0)
 
-        return {"input": padded_tensors, "mask": padding_masks, "conv_len": torch.tensor(conv_len), "scores":scores}
+        return {"input": padded_tensors, "mask": padding_masks, "conv_len": torch.tensor(conv_len), "score":scores}
 
     return _f
 
@@ -78,44 +78,88 @@ if __name__ == "__main__":
     parser.add_argument("--vae_checkpoint", required=True)
     parser.add_argument("--spm_model", required=True)
     parser.add_argument("--data_file", required=True)
+    parser.add_argument("--output_dir", required=True)
+    parser.add_argument("--gpu", type=int, default=0)
+    parser.add_argument("--gru_hidden", type=int, default=128)
+    parser.add_argument("--ff_hidden", type=int, default=128)
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--num_epoch", type=int, default=100)
     args = parser.parse_args()
 
     hyper_param = Path(args.vae_checkpoint).parent / "hyper_param.json"
 
     config = Config()
     config.load_json(str(hyper_param))
+    device = torch.device("cuda:{}".format(args.gpu))
 
     sp = spm.SentencePieceProcessor(model_file=args.spm_model)
+
+    writer = torch.utils.tensorboard.SummaryWriter(log_dir=args.output_dir)
+    with open(str(Path(args.output_dir)/"dbdc_model_param.json"), "wt", encoding="utf-8") as f:
+        json.dump(vars(args), f)
 
     encoder = transformer_Encoder(config, Transformer_Embedding(config), torch.nn.LayerNorm(config.d_model))
 
     checkpoint = torch.load(args.vae_checkpoint, map_location="cpu")
     encoder.load_state_dict(checkpoint["encoder_state_dict"])
-    encoder.eval()
 
     with open(args.data_file, "rt", encoding="utf-8") as f:
         datas = json.load(f)
     dataset = [process_data(item, sp, config.max_len) for item in datas]
     val_size = int(len(dataset)*0.1)
+    print(len(dataset), val_size)
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [len(dataset)-val_size, val_size])
 
     train_dataloader = torch.utils.data.DataLoader(
-            train_dataset, batch_size=64,
-            sampler=torch.utils.data.sampler.RandomSampler(dataset),
+            train_dataset, batch_size=args.batch_size,
+            sampler=torch.utils.data.sampler.RandomSampler(train_dataset),
             collate_fn=get_collate_fn(),
-            num_workers=2, pin_memory=True
+            num_workers=2,
+            pin_memory=True
             )
     val_dataloader = torch.utils.data.DataLoader(
-            val_dataset, batch_size=64,
+            val_dataset, batch_size=args.batch_size,
             shuffle=False,
             collate_fn=get_collate_fn(),
-            num_workers=2, pin_memory=True
+            num_workers=2,
+            pin_memory=True
             )
 
-    model = DBDC(encoder, config.n_latent, 128, 128)
-    model.train()
-    for item in train_dataloader:
-        out = model(item["input"], item["mask"], item["conv_len"])
-        break
-
+    model = DBDC(encoder, config.n_latent, args.gru_hidden, args.ff_hidden)
+    model.to(device)
+    opt = torch.optim.Adam(model.parameters(), lr=args.lr)
     loss_func = torch.nn.MSELoss()
+
+    for i in tqdm.tqdm(range(args.num_epoch)):
+        val_loss = []
+        for item in tqdm.tqdm(val_dataloader):
+            model.eval()
+            out = model(item["input"].to(device), item["mask"].to(device), item["conv_len"].to(device))
+            loss = loss_func(out, item["score"].to(device))
+            val_loss.append(loss.item())
+        writer.add_scalar("val/loss", np.mean(val_loss), i)
+
+        for n, item in enumerate(tqdm.tqdm(train_dataloader)):
+            model.train()
+            out = model(item["input"].to(device), item["mask"].to(device), item["conv_len"].to(device))
+            loss = loss_func(out, item["score"].to(device))
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+            writer.add_scalar("train/loss", loss.item(), i*len(train_dataloader) + n)
+
+        torch.save({
+            "epoch": i,
+            "model_state_dict": model.state_dict(),
+            "opt_state_dict": opt.state_dict()
+            }, str(Path(args.output_dir)/"epoch{:03d}.pt".format(i)))
+
+    val_loss = []
+    for item in tqdm.tqdm(val_dataloader):
+        model.eval()
+        out = model(item["input"], item["mask"], item["conv_len"])
+        loss = loss_func(out, item["score"])
+        val_loss.append(loss.item())
+    writer.add_scalar("val/loss", np.mean(val_loss), i)
+    writer.close()
