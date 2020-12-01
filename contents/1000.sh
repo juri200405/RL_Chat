378d4699d6803a312b226a4cec848fd0c51bcff6
juri200405 <juri200405@gmail.com>
2020-12-01 21:19:51 +0900

    各重み用のスクリプト追加

---
+++ contents/1000.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+~/conda/envs/pl/bin/python	src/run_model.py \
+	-m data/tomioka/spm_model/no_kwdlc.model \
+	-i data/tomioka/no_kwdlc_126.pkl \
+	-p data/tomioka/transformer_config/1000.json \
+	-o data/tomioka/output/transformer_vae/2020_12_01_3/1 \
+	-l data/output/transformer_vae/2020_12_01_3/1
