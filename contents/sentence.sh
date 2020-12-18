21b540abc2b560a67358c7871655c4739cf308d6
juri200405 <juri200405@gmail.com>
2020-12-18 22:44:19 +0900

    VAEとエージェントの使用GPUの統一 & 使用GPUのパラメータ化

--- contents/sentence.sh
+++ contents/sentence.sh
@@ -4,6 +4,7 @@
 	--vae_checkpoint data/tomioka/output/transformer_vae/2020_12_02/2/epoch009.pt \
 	--spm_model data/tomioka/spm_model/no_kwdlc.model \
 	--grammar_data data/tomioka/grammar.json \
-	--output_dir data/tomioka/output/sentence/2020_12_18/2/ \
-	--num_epoch 10000 \
+	--output_dir data/tomioka/output/sentence/2020_12_18/4/ \
+	--num_epoch 1000 \
 	--num_experiment 20 \
+	--gpu 0
