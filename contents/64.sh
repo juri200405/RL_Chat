de4ba83981e9357ec9db806c4f86b955ac2c10b9
juri200405 <juri200405@gmail.com>
2021-01-17 07:20:45 +0900

    スクリプト調整

--- contents/64.sh
+++ contents/64.sh
@@ -3,6 +3,6 @@
 ~/conda/envs/pl/bin/python	src/run_model.py \
 	-m data/tomioka/spm_model/no_kwdlc.model \
 	-i data/tomioka/no_kwdlc_126.pkl \
-	-p data/tomioka/transformer_config/64.json \
-	-o data/tomioka/output/transformer_vae/2020_12_26/1 \
-	-l data/output/transformer_vae/2020_12_26/1
+	-p data/tomioka/output/transformer_vae/2020_12_26/1/hyper_param.json \
+	-o data/tomioka/output/transformer_vae/2021_01_17/1 \
+	-l data/output/transformer_vae/2021_01_17/1
--- contents/board.sh
+++ contents/board.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 ~/conda/envs/pl/bin/tensorboard \
-	--logdir data/output/sentence_change/2021_01_17/ \
+	--logdir data/output/transformer_vae/2021_01_17/ \
 	--port 31096 --bind_all
